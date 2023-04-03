5205df12be9de4c4ba69a9fc0643b7842126bcd2
HermanVFX <HermanVFX@outlook.com>
2023-04-03 10:30:02 +0300

    Initial commit

---
+++ src/main/java/com/hermanvfx/springbackreviewplatform/SpringBackReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hermanvfx.springbackreviewplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackReviewPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackReviewPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hermanvfx/springbackreviewplatform/SpringBackReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermanvfx.springbackreviewplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackReviewPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
