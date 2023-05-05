5bfb4213e5879a3962be81ea5827bed34241694a
Herman <70486410+HermanVFX@users.noreply.github.com>
2023-05-05 16:07:10 +0300

    Update SecurityConfiguration.java

--- src/main/java/com/hermanvfx/springbackreviewplatform/security/SecurityConfiguration.java
+++ src/main/java/com/hermanvfx/springbackreviewplatform/security/SecurityConfiguration.java
@@ -38,6 +38,8 @@ public class SecurityConfiguration {
                     .authorizeHttpRequests()
                     .requestMatchers("/api/v1/auth/**")
                     .permitAll()
+                    .requestMatchers(EndpointRequest.to("prometheus"))
+                    .permitAll()
                     .anyRequest()
                     //.permitAll()
                     .authenticated()
